3ad9dd263a649e571b4e94a147cbf31723fa0a70
juanchavez-snyk <juan.chavez@snyk.io>
2026-07-29 15:03:20 -0600

    fake

---
+++ test.js
@@ -0,0 +1,13 @@
+// Throwaway scratch file. Safe to delete.
+
+function add(a, b) {
+  return a + b
+}
+
+function greet(name) {
+  return `Hello, ${name}!`
+}
+
+console.log(greet('world'), add(2, 3))
+
+module.exports = { add, greet }
